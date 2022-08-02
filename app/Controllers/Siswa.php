bf81aeafb24566d6233c4c163ffcba662f4cf92c
kamilOemam <97325514+kamilOemam@users.noreply.github.com>
2022-08-02 12:42:33 +0700

    Create Siswa.php
    controller siswa

---
+++ app/Controllers/Siswa.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use App\Models\KelasModel;
+use App\Models\SemesterModel;
+use App\Models\SiswaModel;
+use Config\View;
+
+class Siswa extends BaseController
+{
+   public function index()
+   {}
+}
