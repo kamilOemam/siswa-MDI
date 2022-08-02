bc684dbbaf0428b8272bc3442a2cc8f1baf0f46f
kamilOemam <97325514+kamilOemam@users.noreply.github.com>
2022-08-02 20:14:40 +0700

    Update BiodataModel.php
    construct dan fungsi join

--- app/Models/BiodataModel.php
+++ app/Models/BiodataModel.php
@@ -10,4 +10,20 @@ class BiodataModel extends Model
     protected $table            = 'biodata';
     protected $primaryKey       = 'idname';
     protected $allowedFields    = ['nis','umur','citacita','hobi','motivasi','alamat'];
+    
+     protected $db;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->db = \Config\Database::connect();
+    }
+    public function dataBiodata(){
+        $data = $this->db->table('biodata');
+        $data->join('siswa', 'biodata.nis=siswa.nis','LEFT');
+        $result = $data->get()->getResultArray();
+        // dd($result);
+        return $result;
+    }
+
 }
