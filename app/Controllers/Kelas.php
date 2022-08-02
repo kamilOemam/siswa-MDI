c5f72b107e363a8f83da5121d21a13f18d987eae
kamilOemam <97325514+kamilOemam@users.noreply.github.com>
2022-08-02 11:29:51 +0700

    Update Kelas.php
    menambah construk dan  fungsi index

--- app/Controllers/Kelas.php
+++ app/Controllers/Kelas.php
@@ -6,4 +6,22 @@ use App\Controllers\BaseController;
 use App\Models\KelasModel;
 
 
-class Kelas extends BaseController {}
+class Kelas extends BaseController {
+ //pengaktifan model dalam construktor
+    protected $KM;
+    public function __construct()
+    {
+        $this->KM = new KelasModel();
+    }
+    //gettting data
+    public function index()
+    {   
+        $data ['data_kelas']= $this->KM->find();
+        $data['main_title']  = 'Kelas i\'dadiyah';
+        $data['title']  = 'List Kelas';
+
+       
+        return view('layout/kelas/content', $data);//kirimkan data ke view
+    }
+
+}
