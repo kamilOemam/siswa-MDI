6e71ca392918a1694fa09ee826119ceed7cb4685
kamilOemam <97325514+kamilOemam@users.noreply.github.com>
2022-08-02 20:11:34 +0700

    Create FilterAdmin.php
    filter user admin

---
+++ app/Filters/FilterAdmin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class FilterAdmin implements FilterInterface
+{
+    public function before(RequestInterface $request, $arguments = null)
+    {
+        // merintah sebelum login
+        if (session()->idlevel == 0) {
+            return redirect()->to('/login/index');
+        }
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
+    {
+        // setelah login
+        if (session()->idlevel == 1) {
+            return redirect()->to('/home/index');
+        }
+    }
+}
