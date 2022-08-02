dca40d7f2e61fb33639e97c59d309bceb2796bbe
kamilOemam <97325514+kamilOemam@users.noreply.github.com>
2022-08-02 11:05:21 +0700

    menambah assets template
    template

---
+++ public/assets/dist/js/myJS.js
@@ -0,0 +1,10 @@
+$(function () {
+    'use strict';
+
+
+    var listProdi = $(".list-data-prodi");
+    var listSemester = $(".list-data-semester");
+
+    listProdi.dataTable();
+    listSemester.dataTable();
+})
