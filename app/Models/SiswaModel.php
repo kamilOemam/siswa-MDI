3d7312ce77d1fb5689ae350c5ee0cf3eff2300d7
kamilOemam <97325514+kamilOemam@users.noreply.github.com>
2022-08-02 20:20:51 +0700

    Update SiswaModel.php
    construct dan fungsi datasiswa

--- app/Models/SiswaModel.php
+++ app/Models/SiswaModel.php
@@ -10,4 +10,19 @@ class SiswaModel extends Model
     protected $table            = 'siswa';
     protected $primaryKey       = 'nis';
     protected $allowedFields    = ['nis','nama','jk','tmp_lahir','tgl_lahir','idsmt','idkelas','tingkat'];
+    
+     protected $db;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->db = \Config\Database::connect();
+    }
+    public function dataSiswa(){
+        $data = $this->db->table('siswa');
+        $data->join('kelas', 'siswa.idkelas=kelas.idkelas','LEFT');
+        $data->join('semester', 'siswa.idsmt=semester.idsmt','LEFT');
+        $result = $data->get()->getResultArray();
+        return $result;
+    }
 }
