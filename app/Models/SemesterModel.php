c512085b0da90a014daf88119585f415640ed5a2
kamilOemam <97325514+kamilOemam@users.noreply.github.com>
2022-08-02 20:16:29 +0700

    Update SemesterModel.php
    construct dan fungsi

--- app/Models/SemesterModel.php
+++ app/Models/SemesterModel.php
@@ -6,4 +6,21 @@ class SemesterModel extends Model
     protected $useAutoIncrement = true;
     
     protected $allowedFields    = ['smt','idkelas'];
+    
+    protected $db;
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->db = \Config\Database::connect();
+    }
+    public function dataSemester($id = null){
+        $data = $this->db->table('semester');
+        $data->join('kelas', 'semester.idkelas=kelas.idkelas','LEFT');
+        if ($id != null) {
+            $data->where('semester.idkelas', $id);
+        }
+        $result = $data->get()->getResultArray();
+        return $result;
+    }
 }
