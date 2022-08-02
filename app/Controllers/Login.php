4d93b211d95bc13b60526613ed94af05e5dd026b
kamilOemam <97325514+kamilOemam@users.noreply.github.com>
2022-08-02 16:32:27 +0700

    Update Login.php
    menambahkan beberapa fungsi login

--- app/Controllers/Login.php
+++ app/Controllers/Login.php
@@ -6,4 +6,91 @@ use App\Controllers\BaseController;
 use App\Models\ModelLogin;
 
 class Login extends BaseController
-{}
+{
+// function test(){
+    //     echo password_hash('admin', PASSWORD_BCRYPT);
+    // }
+    public function index()
+    {
+        return view('login/index');
+    }
+    public function cekUser()
+    {
+            $iduser =$this->request->getPost('iduser');
+            $pass =$this->request->getPost('pass');
+
+            $validation = \Config\Services::validation();
+
+            $valid = $this->validate([
+                'iduser' =>[
+                    'label'=>'ID User',
+                    'rules'=>'required',
+                    'errors'=>[
+                        'required'=>'{field} tidak boleh kosong'
+                    ]
+                ],
+
+                'pass' =>[
+                    'label'=>'Password',
+                    'rules'=>'required',
+                    'errors'=>[
+                        'required'=>'{field} tidak boleh kosong'
+                    ]
+                ]
+            ]);
+
+           
+            if (!$valid) {
+               $sessError = [
+                   'errIdUser' =>$validation->getError('iduser'),
+                   'errPassword' =>$validation->getError('pass')
+               ]; 
+
+               session()->setFlashdata($sessError);
+               return redirect()->to(site_url('login/index'));
+            }
+            else{
+                $modelLogin = new ModelLogin();
+
+                $cekUserLogin = $modelLogin->find($iduser);
+                if ($cekUserLogin == null) { 
+                    $sessError = [
+                        'errIdUser' =>'Maaf user tidak terdaftar',
+                    ]; 
+     
+                    session()->setFlashdata($sessError);
+                    return redirect()->to(site_url('login/index'));
+                }else{
+                    $passwordUser = $cekUserLogin['userpass'];
+
+                    if (password_verify($pass, $passwordUser)) {
+                        //lanjutkan 
+                        $idlevel = $cekUserLogin['userlevelid'];
+                        $simpan_session = [
+                            'iduser' =>$iduser,
+                            'namauser'=>$cekUserLogin['username'],
+                            'idlevel'=>$idlevel
+                        ];
+                        session()->set($simpan_session);
+                        return redirect()->to('/home/index');
+                    }
+                    else{
+                        
+                        $sessError = [
+                            'errPassword' =>'Maaf password anda salah',
+                        ]; 
+         
+                        session()->setFlashdata($sessError);
+                        return redirect()->to(site_url('login/index'));
+                    }
+                }
+            }
+           
+           
+    }
+    public function keluar()
+    {
+        session()->destroy();
+        return redirect()->to('login/index');
+    }
+}
